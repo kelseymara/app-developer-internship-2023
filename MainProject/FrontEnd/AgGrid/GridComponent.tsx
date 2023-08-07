912214a77e0968b1459ae0ccd2e44bdc8abe84e6
kelseymara <77584252+kelseymara@users.noreply.github.com>
2023-08-07 13:42:16 -0400

    Create GridComponent.tsx

---
+++ MainProject/FrontEnd/AgGrid/GridComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'; // Import React library for building UI components 
+import { AgGridReact } from 'ag-grid-react'; // Import AG Grid React wrapper.
+import 'ag-grid-community/styles/ag-grid.css'; // Import AG Grid base styles.
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Import AG Grid theme styles.
+
+
+// File in progress
