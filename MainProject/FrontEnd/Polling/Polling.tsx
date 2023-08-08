831f73f99757048c4dc6345a849dcbec90e97228
kelseymara <77584252+kelseymara@users.noreply.github.com>
2023-08-08 12:01:31 -0400

    Delete Polling.tsx

--- MainProject/FrontEnd/Polling/Polling.tsx
+++
@@ -1,8 +0,0 @@
-// Snippet of Polling: setting an interval of 10 seconds and calling fetchData every 10 seconds
-useEffect(() => {
-    fetchData(); // Initial Call of fetch Data function (not implemented here)
-
-    const interval = setInterval(fetchData, 10000); // Fetch data every 10 seconds
-
-    return () => clearInterval(interval); // Clean up interval on unmount
-  }, []);
